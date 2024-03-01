e84b85b532396b1cc6f8b2d39d6c2a5e3ec5d180
Md Forhadul Islam <forhadreza1596@gmail.com>
2024-03-01 12:18:09 +0000

    update

--- Models/EcommerceWebAppContext.cs
+++ Models/EcommerceWebAppContext.cs
@@ -4,66 +4,21 @@ using Microsoft.EntityFrameworkCore;
 using System.Data.Entity;
 namespace EcommerceWebApp.Models
 {
-    public class EcommerceWebAppContext
+    public class EcommerceWebAppContext : DbContext
     {
         public EcommerceWebAppContext(DbContextOptions<EcommerceWebAppContext> options) : base(options)
         {
         }
         public System.Data.Entity.DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public System.Data.Entity.DbSet<Cart> Carts { get; set; }
+        public System.Data.Entity.DbSet<Category> Categories { get; set; }
 
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public System.Data.Entity.DbSet<Customer> Customers { get; set; }
+        public System.Data.Entity.DbSet<Order> Orders { get; set; }
 
-        public DbSet<Product> Products { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            
-            modelBuilder.Entity<Product>()
-            .HasOne(p => p.Category)
-            .WithMany(p => p.OrderItem)
-            .HasForeignKey(p => p.CategoryId)
-            .IsRequired();
-
-            modelBuilder.Entity<Category>()
-            .HasOne(c => c.CategoryName)
-            
-            modelBuilder.Entity<OrderItem>()
-                .Haskey(op => new {op.ProductId, op.OrderId, op.ProductOrder});
-
-            modelBuilder.Entity<OrderItem>()
-                .HasOne(op => op.Product)
-                .WithMany()
-                .HasForeignKey(op => op.ProductId);
-
-            modelBuilder.Entity<OrderItem>()
-                .HasOne(op => op.Order).WithMany().HasForeignKey(op => op.OderId);
-
-            modelBuilder.Entity<Cart>()
-                .HasOne(c => c.CartId)
-                .WithOne()
-                .HasForeignKey(c => c.CustomerId);
-
-            modelBuilder.Entity<Order>()
-            .HasOne(o => o.OrderId)
-            .WithMany(o => o.OrderItem)
-            .HasForeignKey(o => o.CustomerId);
- 
-
-            modelBuilder.Entity<Admin>()
-            .HasOne(a => a.AdminId)
-            .WithMany(a => a.Product)
-            .IsRequired();
+        public System.Data.Entity.DbSet<Product> Products { get; set; }
 
 
-
-
-
-
-        }
 
 
     }
