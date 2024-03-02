15c280f42754c35f0f853a11910656a1e0a50b21
Md Forhadul Islam <forhadreza1596@gmail.com>
2024-03-02 12:06:26 +0000

    update

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eshopperz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
