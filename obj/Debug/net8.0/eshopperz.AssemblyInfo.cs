59be40fb3d44a02234cef2535a28e3dbf587ff9f
Md Forhadul Islam <forhadreza1596@gmail.com>
2024-03-04 23:14:47 +0000

    update

--- obj/Debug/net8.0/eshopperz.AssemblyInfo.cs
+++ obj/Debug/net8.0/eshopperz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eshopperz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6341a7007c71b2d884ba435f4051deb67ca96708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b024d0555b312b743c15ebf13b26cd9a490a36c")]
 [assembly: System.Reflection.AssemblyProductAttribute("eshopperz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eshopperz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
